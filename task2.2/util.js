43e27561e0d191420a3d55201c3b16ee339b6083
kolosa <limoyan020@163.com>
2016-09-26 23:33:48 +0800

    Add files via upload

--- task2.2/util.js
+++ task2.2/util.js
@@ -71,3 +71,47 @@ function isMobilePhone(phone) {
     var phonenum=/^1\d{11}$/
     return phonenum.test(phone)
 }
+function addclass(element, newClassName){
+    if (element.className==null||element.className==""){
+        element.className=newClassName;
+    }else{
+        element.className+=''+newClassName;
+    }
+}
+function removeClass(element, oldClassName){
+    var reg=new RegExp('/' + oldClassName + '/');
+    var tempclass=element.className;
+    var newele=tempclass.replace(reg,"").replace(/^\s+|\s+$/,"");
+    element.className=newele
+}
+function isSiblingNode(element, siblingNode){
+    var pare= element.parentElement()
+    var pars= siblingNode.parentElement()
+    if(pare===pars){
+        return true
+    }else{
+        return false
+    }
+}
+function getPosition(element) {
+    var left=element.offsetLeft;
+    var top=element.offsetTop;
+    var par=element.offsetParent()
+    while (par!=null){
+        left += par.offsetLeft;
+        top += par.offsetTop;
+        par = par.offsetParent;
+    }
+    if(document.compatMode=="BackCompat"){
+        var sLeft = document.body.scrollLeft;
+        var sTop = document.body.scrollTop;
+    }else{
+        var sLeft = document.documentElement.scrollLeft;
+        var sTop = document.documentElement.scrollTop;
+        var obj={
+            x:left-sLeft,
+            y:top-sTop
+        }
+        return obj
+    }
+}
