89144bcdf2446b893061e6604d4a1eb36a472804
kolosa <limoyan020@163.com>
2016-09-26 22:39:51 +0800

    Add files via upload

--- task2.2/util.js
+++ task2.2/util.js
@@ -37,6 +37,37 @@ function simpleTrim(str) {
             break
         }
         str=str.substring(i,str.length-1)
-    }return str
+    }
+    for(var i=str.length;i>0; i--){
+        if(str.charAt(i)!=''&&str.charAt(i)!='\t'){
+            break
+        }
+        str=str.substring(0,i)
+    }
+    return str
+}
+function trim(str){
+    return str.replace(/(^\s*)|(\s*$)/g, "");
+}
 
+function each(arr,fn){
+    for(var i in arr){
+        fn(arr[i],i)
+    }
+}
+
+function getObjectLength(obj) {
+    var num=0;
+    for(var i in obj){
+        num++
+    }
+    return num;
+}
+function isEmail(emailStr) {
+    var email=/^([\w\.\-]+)@(\w\.\-])\.([a-z/A-Z]{2,4})$/
+    return email.test(emailStr)
+}
+function isMobilePhone(phone) {
+    var phonenum=/^1\d{11}$/
+    return phonenum.test(phone)
 }
